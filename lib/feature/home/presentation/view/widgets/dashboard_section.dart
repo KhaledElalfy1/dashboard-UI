2620a817e11f4512afbc4279f88a45054342c32f
KhaledElalfy1 <khaledalfy53@gmail.com>
2024-03-07 05:47:33 +0200

    add fl-chart class

--- lib/feature/home/presentation/view/widgets/dashboard_section.dart
+++ lib/feature/home/presentation/view/widgets/dashboard_section.dart
@@ -1,4 +1,5 @@
 import 'package:dashboard/feature/home/presentation/view/widgets/dashboard_activity_details.dart';
+import 'package:dashboard/feature/home/presentation/view/widgets/dashboard_line_chart_card.dart';
 import 'package:dashboard/feature/home/presentation/view/widgets/dashboard_search_bar.dart';
 import 'package:flutter/material.dart';
 
@@ -17,6 +18,10 @@ class DashboardSection extends StatelessWidget {
           height: 20,
         ),
         ActivityDetailsSection(),
+        SizedBox(
+          height: 20,
+        ),
+        LineChartCard(),
         SizedBox(
           height: 20,
         ),
